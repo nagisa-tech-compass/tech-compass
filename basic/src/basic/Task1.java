5a96bb189224edbf99f5e7da812ab2a766dd6bd5
nagisa-tech-compass <nagisa06251998@yahoo.co.jp>
2020-10-27 02:04:33 +0900

    課題提出 - Javaの基礎

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -2,4 +2,9 @@ package basic;
 
 public class Task1 {
 
+	public static void main(String args[]) {
+
+		System.out.println("初めての課題");
+	    System.out.print("Java攻略の道");
+	}
 }
---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,12 @@
+package basic;
+
+class Task2 {
+   public static void main(String[] args){
+
+	   System.out.println("hogehoge");
+	   //文末にはセミコロンをつける
+
+	   System.out.println("hogehoge2");
+	   //mainのなかに作業内容を記入する
+   }
+}
